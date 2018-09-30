3d75b8b8f11f5ec372111df897566057254952af
akihitoChinen <croquette.picker@gmail.com>
2018-09-30 22:35:39 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+	res.send('node api sample');
+	console.log('send response');
+});
+
+app.listen(3000, () => {
+	console.log('start server port:3000');
+});
